d0fe21e70336fb49ccfdeeeef47d6abd8c558dfa
selma <selma@gmail.com>
2017-09-20 11:21:17 +0200

    rajout des commentaires

--- src/app/boucle/boucle.component.ts
+++ src/app/boucle/boucle.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoucleComponent implements OnInit {
   tableau:string[] = ["Golf", "Jaguar", "Mazda", "Peugeot"];
+  
   constructor() { }
 
   ngOnInit() {
